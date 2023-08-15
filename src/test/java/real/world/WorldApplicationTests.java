275fe416f93995efde9880272fcc001df3887893
DH CHOI <bluetifulc@gmail.com>
2023-08-15 19:47:25 +0900

    initial commit

---
+++ src/test/java/real/world/WorldApplicationTests.java
@@ -0,0 +1,13 @@
+package real.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
