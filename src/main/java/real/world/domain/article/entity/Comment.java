de49313833cb4dd340dcd304341317399e7b185e
DH_Choi <58378676+vectorch9@users.noreply.github.com>
2023-08-19 18:51:21 +0900

    [Feature] 엔티티 구현 및 application 설정 파일 작성 (#1)

---
+++ src/main/java/real/world/domain/article/entity/Comment.java
@@ -0,0 +1,56 @@
+package real.world.domain.article.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import real.world.domain.user.entity.User;
+
+@Getter
+@Entity(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String body;
+
+    @CreatedDate
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    protected Comment() {
+    }
+
+    public Comment(Article article, User user, String body) {
+        this.article = article;
+        this.user = user;
+        this.body = body;
+    }
+}
