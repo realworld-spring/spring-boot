c6a6321081e9938523887e1f763ebe56e6efe179
DH CHOI <bluetifulc@gmail.com>
2023-09-12 22:41:39 +0900

    feature: LoginArgumentResolver 구현

--- src/main/java/real/world/domain/auth/annotation/LoginArgumentResolver.java
+++ src/main/java/real/world/domain/auth/annotation/LoginArgumentResolver.java
@@ -3,18 +3,17 @@ package real.world.domain.auth.annotation;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-@Component
 public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(Login.class);
+        return parameter.hasParameterAnnotation(Login.class) &&
+            parameter.getParameterType().equals(Long.class);
     }
 
     @Override
@@ -22,7 +21,7 @@ public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
         NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         final Authentication authentication = SecurityContextHolder.getContext()
             .getAuthentication();
-        return authentication.getPrincipal();
+        return Long.valueOf((String) authentication.getPrincipal());
     }
 
 }
