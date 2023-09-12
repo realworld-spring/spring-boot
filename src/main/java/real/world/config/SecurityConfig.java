df98fd82714bd877258d09f5fa5919cee6816e1a
DH CHOI <bluetifulc@gmail.com>
2023-09-12 20:51:50 +0900

    fix: Merge conflict 해결로 인한 에러 수정

--- src/main/java/real/world/config/SecurityConfig.java
+++ src/main/java/real/world/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package real.world.config;
 
 import java.util.List;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,25 +20,30 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import real.world.security.service.CustomUserDetailsService;
 import real.world.security.authentication.CustomAuthenticationProvider;
-import real.world.security.support.NorRequestMatcher;
+import real.world.security.authentication.CustomUsernamePasswordAuthenticationFilter;
+import real.world.security.handler.CustomAuthenticationFailureHandler;
 import real.world.security.jwt.JwtAuthenticationFilter;
 import real.world.security.jwt.JwtAuthenticationProvider;
-import real.world.security.handler.CustomAuthenticationFailureHandler;
-import real.world.security.authentication.CustomUsernamePasswordAuthenticationFilter;
+import real.world.security.service.UserDetailsByEmailService;
+import real.world.security.service.UserDetailsByIdService;
 import real.world.security.support.JwtUtil;
+import real.world.security.support.NorRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private static final String[] AUTH_PATH = {"/api/users", "/api/users/login"};
-    private static final String[] SWAGGER_PATH = {"/docs/open-api.json", "/swagger-ui/**", "/v3/**"};
+    private static final String LOGIN_PATH = "/api/users/login";
+    private static final String[] AUTH_PATH = {"/api/users", LOGIN_PATH};
+    private static final String[] SWAGGER_PATH = {"/docs/open-api.json", "/swagger-ui/**",
+        "/v3/**"};
 
     private final ObjectPostProcessor<Object> objectPostProcessor;
-    private final CustomUserDetailsService userDetailsByEmailService;
-    private final CustomUserDetailsService userDetailsByIdService;
+
+    private final UserDetailsByEmailService userDetailsByEmailService;
+
+    private final UserDetailsByIdService userDetailsByIdService;
 
     private final JwtUtil jwtUtil;
 
@@ -52,9 +58,11 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, SWAGGER_PATH).permitAll()
                 .anyRequest().hasRole("USER")
             )
-            .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(customAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(jwtAuthenticationFilter(), AuthorizationFilter.class)
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
@@ -80,7 +88,8 @@ public class SecurityConfig {
 
     public AuthenticationManager authenticationManager() throws Exception {
         return new AuthenticationManagerBuilder(objectPostProcessor)
-            .authenticationProvider(new CustomAuthenticationProvider(userDetailsByEmailService, passwordEncoder()))
+            .authenticationProvider(
+                new CustomAuthenticationProvider(userDetailsByEmailService, passwordEncoder()))
             .authenticationProvider(new JwtAuthenticationProvider(userDetailsByIdService, jwtUtil))
             .build();
     }
@@ -89,7 +98,8 @@ public class SecurityConfig {
         return new CustomAuthenticationFailureHandler();
     }
 
-    public CustomUsernamePasswordAuthenticationFilter customAuthenticationFilter() throws Exception {
+    public CustomUsernamePasswordAuthenticationFilter customAuthenticationFilter()
+        throws Exception {
         final CustomUsernamePasswordAuthenticationFilter filter =
             new CustomUsernamePasswordAuthenticationFilter(LOGIN_PATH, authenticationManager());
         filter.setAuthenticationFailureHandler(customAuthenticationFailureHandler());
@@ -98,7 +108,9 @@ public class SecurityConfig {
     }
 
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        final String[] norPath = {AUTH_PATH, LOGIN_PATH};
+        final String[] norPath = Stream.of(AUTH_PATH, SWAGGER_PATH)
+            .flatMap(Stream::of)
+            .toArray(String[]::new);
         final JwtAuthenticationFilter filter = new JwtAuthenticationFilter(
             new NorRequestMatcher(norPath), authenticationManager());
         filter.setAuthenticationFailureHandler(customAuthenticationFailureHandler());
